15cd95feda4a900d4d863214da4291a95689905b
Vitaliy <theyamshikov@yandex.ru>
2022-06-24 11:24:19 +0600

    upd

--- src/Domain/Base/BaseEloquentRepository.php
+++ src/Domain/Base/BaseEloquentRepository.php
@@ -2,32 +2,24 @@
 
 namespace ZnDatabase\Eloquent\Domain\Base;
 
-use Illuminate\Database\Connection;
 use Illuminate\Database\Query\Builder as QueryBuilder;
-use Illuminate\Database\Schema\Builder as SchemaBuilder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Enumerable;
-use ZnCore\Base\Develop\Helpers\DeprecateHelper;
-use ZnCore\Base\EventDispatcher\Traits\EventDispatcherTrait;
-use ZnCore\Domain\Domain\Enums\EventEnum;
-use ZnCore\Domain\Domain\Events\EntityEvent;
-use ZnCore\Domain\Domain\Events\QueryEvent;
 use ZnCore\Domain\Domain\Interfaces\GetEntityClassInterface;
 use ZnCore\Domain\EntityManager\Interfaces\EntityManagerInterface;
-use ZnCore\Domain\Query\Entities\Query;
 use ZnCore\Domain\EntityManager\Traits\EntityManagerAwareTrait;
+use ZnCore\Domain\Query\Entities\Query;
+use ZnCore\Domain\Repository\Traits\MapperTrait;
 use ZnCore\Domain\Repository\Traits\RepositoryDispatchEventTrait;
 use ZnCore\Domain\Repository\Traits\RepositoryQueryTrait;
+use ZnDatabase\Base\Domain\Traits\TableNameTrait;
 use ZnDatabase\Eloquent\Domain\Capsule\Manager;
 use ZnDatabase\Eloquent\Domain\Helpers\QueryBuilder\EloquentQueryBuilderHelper;
 use ZnDatabase\Eloquent\Domain\Traits\EloquentTrait;
-use ZnCore\Domain\Repository\Traits\MapperTrait;
-use ZnDatabase\Base\Domain\Traits\TableNameTrait;
 
 abstract class BaseEloquentRepository implements GetEntityClassInterface
 {
 
-//    use EventDispatcherTrait;
     use EloquentTrait;
     use TableNameTrait;
     use EntityManagerAwareTrait;
@@ -35,26 +27,11 @@ abstract class BaseEloquentRepository implements GetEntityClassInterface
     use RepositoryDispatchEventTrait;
     use RepositoryQueryTrait;
 
-//    protected $autoIncrement = 'id';
-    //private $entityClassName;
-
     public function __construct(EntityManagerInterface $em, Manager $capsule)
     {
         $this->setCapsule($capsule);
         $this->setEntityManager($em);
     }
-
-    /*public function getEntityClass(): string
-    {
-        return $this->entityClassName;
-    }*/
-
-    /*protected function forgeQuery(Query $query = null): Query
-    {
-        $query = Query::forge($query);
-        $this->dispatchQueryEvent($query, EventEnum::BEFORE_FORGE_QUERY);
-        return $query;
-    }*/
 
     protected function forgeQueryBuilder(QueryBuilder $queryBuilder, Query $query)
     {
